4fc4a4698891290ca41c762963e5a08e706ef9cc
mru24 <mru24@yahoo.com>
2024-02-18 10:33:26 +0000

    conf rem

--- www/js/config.js
+++
@@ -1,9 +0,0 @@
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '9af8a7bb2cmshd185d928c7f5a24p131605jsn50ba27212ca6',
-// 		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-// 	}
-// };
-
-const apiKey = "0d9e81eb9ef01befe79a2face57b96e4";
